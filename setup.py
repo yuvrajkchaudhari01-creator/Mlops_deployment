0347ea63c34755f517d451e88b3893bb573842c7
yuvrajkchaudhari01-creator <yuvraj.k.chaudhari01@gmail.com>
2026-03-07 02:18:23 +0530

    project sturcture and env set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Vikash Das",
+    author_email="vikashdas770@gmail.com",
+    packages=find_packages()
+)
